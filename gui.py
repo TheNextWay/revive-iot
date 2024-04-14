59c217bac1930cea243d5bb3da5e566097e4cb66
Arsyadam <work@arsyadam.id>
2024-04-14 17:53:38 +0700

    Items Loop

--- gui.py
+++ gui.py
@@ -4,9 +4,144 @@ import tkinter as tk
 from tkinter import LEFT, RIGHT, Button, PhotoImage, ttk
 import customtkinter as ctk
 from PIL import Image
+import pyglet
+items = {
+    1 : {
+        "id":1,
+        "transaction_id":1,
+        "point":15,
+        "condition":"Good",
+        "clothing_type":"Kaos",
+        "fabric": {
+           1: {
+            "type" : "Katun",
+            "percentage":80
+           },
+           2: {
+            "type" : "Poliester",
+            "percentage":20
+           }
+        }
+    },
+    2 : {
+        "id":2,
+        "transaction_id":1,
+        "point":15,
+        "condition":"Good",
+        "clothing_type":"Celana",
+        "fabric": {
+            1: {
+                "type" : "Katun",
+                "percentage":100
+            },
+        }
+    },
+    3 : {
+        "id":3,
+        "transaction_id":1,
+        "point":15,
+        "condition":"Good",
+        "clothing_type":"Kain",
+        "fabric": {
+            1: {
+                "type" : "Katun",
+                "percentage":100
+            },
+        }
+    },
+    4 : {
+        "id":4,
+        "transaction_id":1,
+        "point":20,
+        "condition":"Good",
+        "clothing_type":"Jaket",
+        "fabric": {
+            1: {
+                "type" : "Katun",
+                "percentage":100
+            },
+        }
+    },
+    5 : {
+        "id":5,
+        "transaction_id":1,
+        "point":30,
+        "condition":"Good",
+        "clothing_type":"Kaos",
+        "fabric": {
+            1: {
+                "type" : "Katun",
+                "percentage":100
+            },
+        }
+    },
+    6 : {
+        "id":5,
+        "transaction_id":1,
+        "point":30,
+        "condition":"Good",
+        "clothing_type":"Kaos",
+        "fabric": {
+            1: {
+                "type" : "Katun",
+                "percentage":100
+            },
+        }
+    },
+    7 : {
+        "id":5,
+        "transaction_id":1,
+        "point":30,
+        "condition":"Good",
+        "clothing_type":"Kaos",
+        "fabric": {
+            1: {
+                "type" : "Katun",
+                "percentage":100
+            },
+        }
+    },
+    8 : {
+        "id":5,
+        "transaction_id":1,
+        "point":30,
+        "condition":"Good",
+        "clothing_type":"Kaos",
+        "fabric": {
+            1: {
+                "type" : "Katun",
+                "percentage":100
+            },
+        }
+    },
+    9 : {
+        "id":5,
+        "transaction_id":1,
+        "point":30,
+        "condition":"Good",
+        "clothing_type":"Kaos",
+        "fabric": {
+            1: {
+                "type" : "Katun",
+                "percentage":100
+            },
+        }
+    },
+}
 
+
+tsm={
+   "coordinate": "-7.938878, 112.579912",
+   "status": "ON",
+   "ip_address": "108.122.028",
+   "temperature": "35",
+   "capacity": "50",
+   "max_capacity": "150",
+   "cs_contact": "082245676151",
+}
 ctk.set_appearance_mode("light")
 
+pyglet.font.add_file('assets/PlusJakartaSans-SemiBold.ttf')
 window = tk.Tk()
 
 window.geometry("1280x720")
@@ -20,13 +155,13 @@ frame_info.pack(pady=40,
                 side="top")
 
 
-logo_image = ctk.CTkImage(light_image=Image.open("assets/frame0/logo.png"),
-                        dark_image=Image.open("assets/frame0/logo-d.png"),
-                        size=(254, 61))
-
 wa_icon = ctk.CTkImage(light_image=Image.open("assets/frame0/whatsapp.png"),
                         dark_image=Image.open("assets/frame0/whatsapp-d.png"),
                         size=(25, 25))
+
+logo_image = ctk.CTkImage(light_image=Image.open("assets/frame0/logo.png"),
+                        dark_image=Image.open("assets/frame0/logo-d.png"),
+                        size=(254, 61))
 
 temperature_icon = ctk.CTkImage(light_image=Image.open("assets/frame0/temperature.png"),
                                 dark_image=Image.open("assets/frame0/temperature-d.png"),
@@ -36,9 +171,24 @@ capacity_icon = ctk.CTkImage(light_image=Image.open("assets/frame0/database.png"
                             dark_image=Image.open("assets/frame0/database-d.png"),
                             size=(25, 25))
 
-font_info = ctk.CTkFont(family="Plus Jakarta Sans", 
+shirt = ctk.CTkImage(Image.open("assets/frame0/shirt.png"),
+                        size=(75, 75))
+
+pants = ctk.CTkImage(Image.open("assets/frame0/pants.png"),
+                        size=(75, 75))
+
+jacket = ctk.CTkImage(Image.open("assets/frame0/jacket.png"),
+                        size=(75, 75))
+
+fabric = ctk.CTkImage(Image.open("assets/frame0/fabric.png"),
+                        size=(75, 75))
+
+
+font_info = ctk.CTkFont(family="Plus Jakarta Sans Semibold", 
                         size=20,
                         weight='bold')  
+
+
 
 ctk.CTkLabel(
     frame_info, 
@@ -76,24 +226,9 @@ ctk.CTkLabel(
 textile = ctk.CTkImage(Image.open("assets/frame0/textiles.png"),
                                   size=(564, 232.73))
 
-
-
-
-button_image_1 = PhotoImage(
-    file="assets/frame0/button_1.png")
-
-button_1 = Button(
-    image=button_image_1,
-    borderwidth=0,
-    highlightthickness=0,
-    command=lambda: print("button_1 clicked"),
-    relief="flat"
-)
-
 frame_action = ttk.Frame(window, 
                        width=960, 
-                       height=900,
-                       )
+                       height=900)
 
 frame_action.pack(padx=50, side="left")
 
@@ -102,17 +237,62 @@ ctk.CTkLabel(frame_action, image=textile, text="").pack()
 def button_event():
     print("button pressed")
 
-button = ctk.CTkButton(frame_action,hover=True,hover_color="white", text="", image=button_image_1, command=button_event, fg_color='transparent', )
-button.pack(pady=40)
 
-items_frame = ttk.Frame(window, borderwidth=5, relief="ridge", width=960, height=830)
-items_frame.pack()
+ctk.CTkButton(frame_action, corner_radius=20,command=button_event(), text="Selesai Menyetor", font=('Plus Jakarta Sans Semibold',48), fg_color="#1BAE80",hover_color="#0B7156",  text_color="#F6FAF7", height=115, width=740).pack(pady=80)
+        
 
-frame_item = ttk.Frame(items_frame, borderwidth=20, relief="ridge", width=535, height=155)
-frame_item.pack()
+items_frame = ttk.Frame(window, borderwidth=5,width=960, height=830)
+items_frame.pack(anchor="center")
 
-# def item(item):
 
+text_item = ctk.CTkFont(family="Plus Jakarta Sans Semibold", 
+                        size=64)  
+
+
+
+
+def item_component(item):
+    frame_item = ttk.Frame(items_frame, borderwidth=10,  width=535, height=155)
+    frame_items = ttk.Frame(frame_item,  width=535, height=100)
+
+    match item["clothing_type"]:
+        case "Kaos":
+            ctk.CTkLabel(frame_item, image=shirt, text="").pack(side="left", anchor="nw", pady=25)
+            ctk.CTkLabel(frame_items,text_color="#2D3648", font=('Plus Jakarta Sans Semibold',64), text=(1,"x", item["clothing_type"])).pack(anchor="nw")
+
+            
+        case "Celana":
+            ctk.CTkLabel(frame_item, image=pants, text="").pack(side="left", anchor="nw", pady=25)
+            ctk.CTkLabel(frame_items,text_color="#2D3648", font=('Plus Jakarta Sans Semibold',64), text=(1,"x", item["clothing_type"])).pack(anchor="nw")
+
+            
+        case "Jaket":
+            ctk.CTkLabel(frame_item, image=jacket, text="").pack(side="left", anchor="nw", pady=25)
+            ctk.CTkLabel(frame_items,text_color="#2D3648", font=('Plus Jakarta Sans Semibold',64), text=(1,"x", item["clothing_type"])).pack(anchor="nw")
+
+
+        case "Kain":
+            ctk.CTkLabel(frame_item, image=fabric, text="").pack(side="left", anchor="nw", pady=25)
+            ctk.CTkLabel(frame_items,text_color="#2D3648", font=('Plus Jakarta Sans Semibold',64), text=(1,"x", item["clothing_type"])).pack(anchor="nw")
+
+
+    
+    
+    frame_fabric_type = ttk.Frame(frame_items, width=535, height=155)
+
+    for x in item["fabric"]:
+        ctk.CTkButton(frame_fabric_type, text=(item["fabric"][x]["percentage"],"%", item["fabric"][x]["type"]), font=('Plus Jakarta Sans Semibold',14), border_color="#2D3648",fg_color="transparent",border_width=2, hover=False, text_color="#2D3648", height=30).pack(side="right", anchor='w',padx=5)
+        frame_fabric_type.pack(anchor="w")
+
+
+    frame_item.pack(anchor="nw")
+    frame_items.pack(anchor="nw", padx=20)
+
+
+
+for item in items.items():
+  item_component(item[1])
+  
 
 window.resizable(False, False)
 window.attributes('-fullscreen', True)
--- main.py
+++ main.py
@@ -1,91 +1,90 @@
-import cv2
-import numpy as np
-import orangepi.3
-   from OPi import GPIO
+# import cv2
+# import numpy as np
+# from time import sleep
 
-   GPIO.setmode(orangepi.3.BOARD)
-from time import sleep
-import os
+# import OPi as GPIO
+# import orangepi.3 as
+# import os
 
-GPIO.setmode(orangepi3.BOARD)
-GPIO.setup(8, GPIO.OUT)
+# GPIO.setmode(orangepi.3.BOARD) 
+# GPIO.setup(8, GPIO.OUT)
 
-# Capture video
-def stop_iot() :
-    sleep(1.5)
-    GPIO.
-    print("Conveyor Stop")
-    sleep(1.5)
-video_capture = cv2.VideoCapture(0)  # Change to your video source (0 for webcam)
+# # Capture video
+# def stop_iot() :
+#     sleep(1.5)
+#     GPIO.
+#     print("Conveyor Stop")
+#     sleep(1.5)
+# video_capture = cv2.VideoCapture(0)  # Change to your video source (0 for webcam)
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (1280, 720))  # Adjust resolution if needed
+# # Define the codec and create VideoWriter object
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('output.avi', fourcc, 20.0, (1280, 720))  # Adjust resolution if needed
 
-# Read first frame and convert to grayscale
-ret, prev_frame = video_capture.read()
-prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-photo_directory = "detected_photos"
-if not os.path.exists(photo_directory):
-    os.makedirs(photo_directory)
+# # Read first frame and convert to grayscale
+# ret, prev_frame = video_capture.read()
+# prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+# photo_directory = "detected_photos"
+# if not os.path.exists(photo_directory):
+#     os.makedirs(photo_directory)
 
-movement_detected = False  # Variable to track movement detection
-photo_counter = 0
-while True:
-    ret, frame = video_capture.read()
-    if not ret:
-        break
-    # GPIO.output(18, 1)
-    # Convert frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# movement_detected = False  # Variable to track movement detection
+# photo_counter = 0
+# while True:
+#     ret, frame = video_capture.read()
+#     if not ret:
+#         break
+#     # GPIO.output(18, 1)
+#     # Convert frame to grayscale
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    # Calculate absolute difference between current frame and previous frame
-    frame_diff = cv2.absdiff(gray, prev_gray)
+#     # Calculate absolute difference between current frame and previous frame
+#     frame_diff = cv2.absdiff(gray, prev_gray)
     
-    # Apply thresholding
-    _, thresh = cv2.threshold(frame_diff, 25, 255, cv2.THRESH_BINARY)
+#     # Apply thresholding
+#     _, thresh = cv2.threshold(frame_diff, 25, 255, cv2.THRESH_BINARY)
     
-    # Find contours
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#     # Find contours
+#     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    # Filter contours by area and detect movement
-    min_area = 100
-    for contour in contours:
-        if cv2.contourArea(contour) > min_area:
-            x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            movement_detected = True  # Set movement detected to True
+#     # Filter contours by area and detect movement
+#     min_area = 100
+#     for contour in contours:
+#         if cv2.contourArea(contour) > min_area:
+#             x, y, w, h = cv2.boundingRect(contour)
+#             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+#             movement_detected = True  # Set movement detected to True
     
-    # If movement is detected, run a command in terminal
-    if movement_detected:
-        # sleep(1.5)
-        stop_iot()
-        # photo_name = os.path.join(photo_directory, f"movement_{photo_counter}.jpg")
-        # cv2.imwrite(photo_name, frame)
-        # print(f"Movement detected. Photo saved as {photo_name}")
-        # photo_counter += 1
-        # GPIO.output(18, 0)
-        movement_detected = False  # Reset movement detection
+#     # If movement is detected, run a command in terminal
+#     if movement_detected:
+#         # sleep(1.5)
+#         stop_iot()
+#         # photo_name = os.path.join(photo_directory, f"movement_{photo_counter}.jpg")
+#         # cv2.imwrite(photo_name, frame)
+#         # print(f"Movement detected. Photo saved as {photo_name}")
+#         # photo_counter += 1
+#         # GPIO.output(18, 0)
+#         movement_detected = False  # Reset movement detection
     
-    # Write the frame to the output video
-    out.write(frame)
+#     # Write the frame to the output video
+#     out.write(frame)
     
-    # Display frame
-    cv2.imshow('Fabric Movement Detection', frame)
+#     # Display frame
+#     cv2.imshow('Fabric Movement Detection', frame)
     
-    # Update previous frame
-    prev_gray = gray
+#     # Update previous frame
+#     prev_gray = gray
     
-    # Press 'q' to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#     # Press 'q' to quit
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-# Release video capture, writer, and close windows
+# # Release video capture, writer, and close windows
     
-
-
 
 
-video_capture.release()
-out.release()
-cv2.destroyAllWindows()
+
+
+# video_capture.release()
+# out.release()
+# cv2.destroyAllWindows()
